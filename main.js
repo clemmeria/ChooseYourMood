dcc61556442b64f7753db4120b7af5a00f619e81
clementine <clemmeria@hotmail.fr>
2017-12-13 17:55:55 +0100

    ajout key

--- main.js
+++ main.js
@@ -4,6 +4,8 @@
 https://www.googleapis.com/webfonts/v1/webfonts?key=AIzaSyCq2Ygc98wsvGDk4XY_ApI4CoXNPu__Q5Y
 */
 
+/*
+// Exemple de requête trouvé sur le guide pour créer une API Google Fonts
 var apiUrl = [];
 apiUrl.push('https://fonts.googleapis.com/css?family=');
 apiUrl.push(anonymousPro.family.replace(/ /g, '+'));
@@ -18,4 +20,22 @@ if (contains('greek', anonymousPro.subsets)) {
 
 // url: 'https://fonts.googleapis.com/css?family=Anonymous+Pro:italic&subset=greek'
 var url = apiUrl.join('');
+*/
+
+// // // Création d'une classe pour les stats : // // // 
+var request = new XMLHttpRequest();
+var classes;
+
+request.onreadystatechange = function () {
+    if (request.readyState == 4 && request.status == 200) {
+        console.log(request.responseText);
+        classes = JSON.parse(request.responseText);
+    }
+}
+
+request.open('GET', 'https://www.googleapis.com/webfonts/v1/webfonts?key=AIzaSyCq2Ygc98wsvGDk4XY_ApI4CoXNPu__Q5Y/', true);
+request.send();
+
+console.log("Ok");
+var nom=classes.family;
 
