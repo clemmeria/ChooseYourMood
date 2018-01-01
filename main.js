ad0a98a176b07ec99b3662af8d2b7d85ac933c1f
LauraLSPG <laura.lassuspigat@gmail.com>
2018-01-01 20:41:06 +0100

    Debut de JS qui ne fonctionne pas

--- main.js
+++ main.js
@@ -23,7 +23,7 @@ var url = apiUrl.join('');
 */
 
 // // // Création d'une classe pour les stats : // // // KESKECE?
-var request = new XMLHttpRequest();
+/*var request = new XMLHttpRequest();
 var classes;
 
 request.onreadystatechange = function () {
@@ -37,5 +37,86 @@ request.open('GET', 'https://www.googleapis.com/webfonts/v1/webfonts?key=AIzaSyC
 request.send();
 
 console.log("Ok");
-var nom=classes.family;
+var nom=classes.family;*/
 
+
+
+/*_____MENU : fox-emoticons_____*/
+
+var $moods = document.querySelectorAll(".mood-child");
+
+for(var image of $moods) {
+	console.log(image.alt);
+}
+
+function setSelected(event) {
+    var $oldMood = document.querySelector(".mood-child-selected");
+	$oldMood.classList.remove("mood-child-selected"); //Ces deux lignes fonctionnaient jusquà ce que jajoute les suivantes qui annulent leur effet
+
+	//("$"+this.alt).classList.add("mood-child-selected");
+	(event.target).classList.add("mood-child-selected");
+	console.log(event.target); // NI this NI event.target NE FONCTIONNENT
+	var $newMood = document.querySelector(".mood-child-selected");
+	//console.log($newMood.classList);
+
+	/*switch(this.alt) {
+		case "neutral":
+			break;
+
+		case "3D":
+			
+			break;
+
+		case "web":
+			
+			break;
+
+		case "cinema":
+			
+			break;
+
+			default:
+	}*/
+
+	/*for(var image of $moods) {
+		console.log($moods.classList);
+		image.classList.add("mood-child-selected");
+	}*/
+
+}
+
+/*$moods.forEach(function(mood) {
+ 	mood.addEventListener("click", setSelected(this))});*/
+
+/* for(var image of $moods) {
+ 	image.addEventListener("click", setSelected(this))});*/
+
+var $neutral = document.querySelector("#neutral");
+$neutral.addEventListener("click", setSelected());
+var $happy = document.querySelector("#happy");
+$happy.addEventListener("click", setSelected());
+var $sad = document.querySelector("#sad");
+$sad.addEventListener("click", setSelected());
+var $angry = document.querySelector("#angry");
+$angry.addEventListener("click", setSelected());
+var $surprised = document.querySelector("#surprised");
+$surprised.addEventListener("click", setSelected());
+var $tired = document.querySelector("#tired");
+$tired.addEventListener("click", setSelected());
+
+
+
+/*
+var $moodSelected = document.querySelector("#mood-child-selected");;
+var $moodDisplayed;
+
+function setMenu(event) {
+	
+}
+
+$moods.addEventListener("click", setMenu);
+
+function setSelected(event) {
+	//$moodSelected.src = $moodDisplayed.src;
+}
+*/
