ef450aaaafe554cc2052cb7582f91a3090cd4704
clementine <clemmeria@hotmail.fr>
2018-01-07 13:13:41 +0100

    creation de fonctions

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-// APpel AJAX GET
+// Appel AJAX GET
 // Prend en paramètres l'URL cible et la fonction callback appelée en cas de succès
 
 function ajaxGet(url, callback) {
@@ -20,85 +20,101 @@ function ajaxGet(url, callback) {
 
 // Accès à l'API Google Fonts avec la clé d'accès AIzaSyCq2Ygc98wsvGDk4XY_ApI4CoXNPu__Q5Y
 ajaxGet("https://www.googleapis.com/webfonts/v1/webfonts?key=AIzaSyCq2Ygc98wsvGDk4XY_ApI4CoXNPu__Q5Y", function (reponse) {
+
     var fonts = JSON.parse(reponse);
 
     // Appel de la fonction setSelected
     $imgs = document.querySelectorAll(".mood-child");
     for (let img of $imgs) {
         img.addEventListener("click", setSelected);
+        var objetEvent = img.addEventListener("click", setSelected);
     }
 
-    // Création des spans qui vont recueillir les infos typographiques à insérer dans la pop-up
-    var famille = document.createElement("p");
-    var categorie = document.createElement("p");
+    // Il faut réussir à envoyer l'objet event à la fonction searchTypo
+    // Cette objet est dans la fonction setSelected
 
-    function setSelected(e) {
-        let $oldImg = document.querySelector(".mood-child-selected");
-        if ($oldImg)
-        	$oldImg.classList.remove("mood-child-selected");
+    // Appel de la fonction searchTypo avec comme arguments event et fonts
+    //searchTypo(objetEvent,fonts);
 
-        let $newImg = this;
-        $newImg.classList.add("mood-child-selected");
-
-        //Apparition de la fenêtre pop-up contenant l'image du mood et le texte
-        var $popUpText = document.querySelector("#pop-up-child");
-		var $popUpImg = document.querySelector("#pop-up-img");
-		$popUpImg.alt = $newImg.alt;
-		$popUpImg.src = $newImg.src;
-        document.querySelector("#pop-up-parent").style.visibility = "visible";
-
-		//Changement du menu de selection par modification de class
-		for(let img of $imgs) {
-			img.classList.add("mood-child-selection");
-		}
-		var moodParent = document.querySelector(".mood-parent");
-		moodParent.classList.add("mood-parent-selection");
-
-        // Recherche du mood selectionné et attribution de la font
-        var font = "";
-        switch (e.target.id) {
-            case "neutral":
-                font = "Bitter";
-                break;
-            case "happy":
-                font = "Lobster";
-                break;
-            case "sad":
-                font = "Frijole";
-                break;
-            case "angry":
-                font = "Bangers";
-                break;
-            case "surprised":
-                font = "Amatic SC";
-                break;
-            case "tired":
-                font = "Love Ya Like A Sister";
-                break;
-            default:
-                font = "Lato";
-        }
-
-        // Changement de la font
-        $popUpText.style.fontFamily = font;
-
-        // Recherche d'informations sur la font : famille et catégorie
-        var items = fonts.items;
-        var family = font;
-        var variants = items.variants;
-        for(var i = 0; i < items.length; i++) {
-            if(font == items[i].family) {
-                category = items[i].category;
-            }
-        }
-        
-        // Modification du contenu pour ajouter les infos typographiques
-        famille.textContent = "Family : " + family;
-        categorie.textContent = "Category : " + category;
-        var infos = document.querySelector("#pop-up-text");
-        
-        // Ajout des infos en tant qu'enfant au paragraphe pop-up-text
-        infos.appendChild(famille);
-        infos.appendChild(categorie);
-    }
 });
+
+// Création des spans qui vont recueillir les infos typographiques à insérer dans la pop-up
+var famille = document.createElement("p");
+var categorie = document.createElement("p");
+
+function setSelected(e) {
+
+    let $oldImg = document.querySelector(".mood-child-selected");
+    if ($oldImg)
+        $oldImg.classList.remove("mood-child-selected");
+
+    let $newImg = this;
+    $newImg.classList.add("mood-child-selected");
+
+    //Apparition de la fenêtre pop-up contenant l'image du mood et le texte
+    var $popUpText = document.querySelector("#pop-up-child");
+    var $popUpImg = document.querySelector("#pop-up-img");
+    $popUpImg.alt = $newImg.alt;
+    $popUpImg.src = $newImg.src;
+    document.querySelector("#pop-up-parent").style.visibility = "visible";
+
+    //Changement du menu de selection par modification de class
+    for (let img of $imgs) {
+        img.classList.add("mood-child-selection");
+    }
+    var moodParent = document.querySelector(".mood-parent");
+    moodParent.classList.add("mood-parent-selection");
+
+    // return (e);
+}
+
+function searchTypo(e, fonts) {
+
+    // Recherche du mood selectionné et attribution de la font
+    var font = "";
+    switch (e.target.id) {
+        case "neutral":
+            font = "Bitter";
+            break;
+        case "happy":
+            font = "Lobster";
+            break;
+        case "sad":
+            font = "Frijole";
+            break;
+        case "angry":
+            font = "Bangers";
+            break;
+        case "surprised":
+            font = "Amatic SC";
+            break;
+        case "tired":
+            font = "Love Ya Like A Sister";
+            break;
+        default:
+            font = "Lato";
+    }
+
+    // Changement de la font
+    $popUpText.style.fontFamily = font;
+
+    // Recherche d'informations sur la font : famille et catégorie
+    var items = fonts.items;
+    var family = font;
+    var variants = items.variants;
+    for (var i = 0; i < items.length; i++) {
+        if (font == items[i].family) {
+            category = items[i].category;
+        }
+    }
+
+    // Modification du contenu pour ajouter les infos typographiques
+    famille.textContent = "Family : " + family;
+    categorie.textContent = "Category : " + category;
+    var infos = document.querySelector("#pop-up-text");
+
+    // Ajout des infos en tant qu'enfant au paragraphe pop-up-text
+    infos.appendChild(famille);
+    infos.appendChild(categorie);
+
+}
